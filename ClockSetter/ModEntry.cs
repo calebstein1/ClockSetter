2a7fe37d0fa8f230c05812eed108784a1d505b19
Caleb Stein <caleb@calebstein.net>
2024-06-10 08:42:40 -0700

    add settings panel

--- ClockSetter/ModEntry.cs
+++ ClockSetter/ModEntry.cs
@@ -1,4 +1,6 @@
-﻿using StardewModdingAPI;
+﻿using System.Threading.Tasks.Sources;
+using StardewModdingAPI;
+using StardewValley;
 
 namespace ClockSetter;
 
@@ -6,6 +8,28 @@ internal sealed class ModEntry : Mod
 {
     public override void Entry(IModHelper helper)
     {
-        
+        var timeOfDay = Game1.timeOfDay;
+
+        helper.Events.GameLoop.GameLaunched += (sender, e) =>
+        {
+            var configMenu = helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null) return;
+
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => timeOfDay = Game1.timeOfDay,
+                save: () => Game1.timeOfDay = timeOfDay
+            );
+
+            configMenu.AddNumberOption(
+                mod: ModManifest,
+                name: () => "Hour",
+                getValue: () => Game1.timeOfDay,
+                setValue: value => timeOfDay = value,
+                min: 600,
+                max: 2600,
+                interval: 10
+            );
+        };
     }
 }
